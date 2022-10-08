d3d6f2e6c277b92430ce05568ab17ab6e2ebccf8
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-07 21:57:46 -0700

    created initial functions and console logs

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowercase = uppercase.map(letter => letter.toLowerCase())
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
+
+function generatePassword(){}
+//Step 1 ask user how many characters?
+var passwordLength = promt('How many charcters would you like your password to be?', '12') 
+console.log(passwordLength)
+//step 2 ask user to use uppercase?
+var useUppercase = confirm('Would you like to include uppercase letters?')
+console.log(useUppercase)
+//step 3 ask uder to use lowercase? 
+var useLowercase = confirm('Would you like to include lowercase letters?')
+console.log(useLowercase)
+//step 4 ask user to use numbers?
+var useNumbers = confirm('Would you like to include numbers?')
+console.log(useNumbers) 
+//step 5 ask user to use special character?
+var useSpecials = confirm('Would you like to include special characters?')
+console.log(useSpecials) 
