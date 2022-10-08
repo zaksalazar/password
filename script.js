112d29ba0bdc3da7cb900be2774a1423e672d2ab
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-07 21:38:45 -0700

    added variables to .js

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Alphabet, Numbers, Special Characters
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = uppercase.map(letter => letter.toLowerCase())
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
