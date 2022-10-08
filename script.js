6e1398686f6f539685e2f40d79b01e627f825525
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-07 22:49:59 -0700

    worked on function

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -19,9 +18,13 @@ var lowercase = uppercase.map(letter => letter.toLowerCase())
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
 
-function generatePassword(){}
+function getRandomInt(max) {
+  math.random ()
+     return math.ceil(math.random() * max)
+}
+function generatePassword(){
 //Step 1 ask user how many characters?
-var passwordLength = promt('How many charcters would you like your password to be?', '12') 
+var passwordLength = promt('How many charcters would you like your password to be?' (8-128), '12') 
 console.log(passwordLength)
 //step 2 ask user to use uppercase?
 var useUppercase = confirm('Would you like to include uppercase letters?')
@@ -37,7 +40,7 @@ var useSpecials = confirm('Would you like to include special characters?')
 console.log(useSpecials) 
 
 //arrange selected numbers and characters that can be randomized 
-var potentialCharacters =[]
+var potentialCharacters = []
 if (useUppercase) {
     potentialCharacters = potentialCharacters.concat(uppercase)
 }
@@ -51,5 +54,15 @@ if (useNumbers) {
 }
 
 if (useSpecials) {
-    potentialCharacters = potentialCharacters.concat(specials)
+    potentialCharacters = potentialCharacters.concat(special)
+}
+
+//create the password length 
+
+var password = " "
+for (var i = 0; i < passwordLength; i ++) {
+  password = password + potentialCharacters[Math.random(potentialCharacters.length)]
+}
+
+return password 
 }
